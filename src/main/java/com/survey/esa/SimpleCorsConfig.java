cd7d9c522c79f88c93257802818d50b54a8ba8ac
Aymen <aymen@Aymens-MacBook-Air.local>
2025-07-27 15:37:22 +0530

    added cors config

---
+++ src/main/java/com/survey/esa/SimpleCorsConfig.java
@@ -0,0 +1,16 @@
+package com.survey.esa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SimpleCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("*"); // Allow all methods (GET, POST, etc.)
+    }
+}
